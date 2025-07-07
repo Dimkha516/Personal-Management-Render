60b6f0420572fe87071f2bc0f9ff6887240fd1fe
bamba <dimkha516@gmail.com>
2025-07-07 11:15:36 +0000

    Add chef service to services table

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'chef_service_id'];
 
     public function employes()
     {
         return $this->hasMany(Employe::class);
+    }
+
+    public function chef()
+    {
+        return $this->belongsTo(Employe::class, 'chef_service_id');
     }
 }
