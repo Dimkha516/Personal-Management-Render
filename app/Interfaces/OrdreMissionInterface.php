2f3978848d4ca5fb45fe18eca9d559c208fad0a1
bamba <dimkha516@gmail.com>
2025-08-20 17:17:23 +0000

    remove motif to valide conge

---
+++ app/Interfaces/OrdreMissionInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Interfaces;
+
+interface OrdreMissionInterface {
+    public function getAll();
+    public function getById(int $id);
+    public function store(array $data);
+    public function update(int $id, array $data);
+     public function destroy(int $id);
+}
